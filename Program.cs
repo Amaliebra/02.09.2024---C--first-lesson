83b162115ee4326a407e3777e9e8cf7f11d93cda
amaliebra <jba082024@gmail.com>
2024-09-02 10:57:20 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _02._09._2024___C__first_lesson;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string helloWorld = "Hello World";
+
+        int myNumber = 10;
+
+        double myDecimal = 10.25;
+
+        Console.WriteLine(helloWorld);
+        Console.WriteLine(myNumber + 10.5);
+        Console.WriteLine(myDecimal);
+
+        Console.WriteLine("Hei, hva heter du?");
+        string? navn = Console.ReadLine();
+        Console.WriteLine("Hei" + navn);
+        
+        char a = '@';
+        char b = 'B';
+        Console.WriteLine(a + b);
+    }
+}
